aca3717d5f64bf8843d566c7f07500ade21ac929
limbado <limbado23@gmail.com>
2025-08-05 11:42:45 +0100

    adicionadas novas funções
    adicionadas as funções que quero para o programa

--- ParqueCodigo.py
+++ ParqueCodigo.py
@@ -22,3 +22,28 @@ def registar_carro():
             print(f"Carro {mat} no lugar {i} às {agora.strftime('%H:%M')}")
             return
     print("Parque cheio!")
+
+def marcar_pago():
+    global receita
+    mat = input("Matrícula: ").upper()
+    for info in lugares.values():
+        if info and info['matricula'] == mat and not info['pago']:
+            tempo = datetime.now() - info['entrada']
+            horas = math.ceil(tempo.total_seconds() / 3600)
+            preco = horas * 1.35
+            info['pago'] = True
+            info['preco'] = preco
+            receita += preco
+            print(f"{mat}: {horas}h x 1.35€ = {preco:.2f} €")
+            return
+    print("Não encontrada ou já paga.")
+
+def marcar_pago(): #funçao para marcar os que ja pagaram, tenho que fazer com que peça a matricula e que calcule as horas e preço
+
+def listar_nao_pagos(): #função para marcar os que nao pagaram, tenho que fazer com que peça a matricula e mostre as horas e quando nao estao pagos
+
+def remover_carro(): #função para removar os careros que ja pagaram e trocar automaticamente para None
+
+def mostrar_receita(): #função para mostrar quanto o parque deu no total (acrescentar o diario e mensal, ou semanal até)
+
+def menu(): 
