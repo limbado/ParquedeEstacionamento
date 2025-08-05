6d040f8b9555bd7b4be08458a708554ffc2ef2e0
limbado <limbado23@gmail.com>
2025-08-05 10:20:06 +0100

    Função mostrar_lugares
    Adicionada função mostrar_lugares, serve para dar ao utilizador uma visão geral do estado do parque, facilitando o controlo dos lugares ocupados, livres e ds pagamentos

--- ParqueCodigo.py
+++ ParqueCodigo.py
@@ -1,6 +1,14 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import math
 
 TOTAL_LUGARES = 83
 lugares = {i: None for i in range(1, TOTAL_LUGARES + 1)}
 receita_total = 0.0
+
+def mostrar_lugares():
+    for i, info in lugares.items():
+        if info:
+            estado = "Pago" if info['pago'] else "Por pagar"
+            print(f"Lugar {i}: {info['matricula']} | Entrada: {info['entrada'].strftime('%d/%m %H:%M')} | {estado} | {info['preco']:.2f} €")
+        else:
+            print(f"Lugar {i}: [LIVRE]")
