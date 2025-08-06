e6ee4550a2cd27aa914835c903fe64bfe0974222
limbado <limbado23@gmail.com>
2025-08-06 10:00:03 +0100

    Interface Visual adicionada

--- ParqueCodigo.py
+++ ParqueCodigo.py
@@ -1,5 +1,7 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 import math
+import tkinter as tk
+from tkinter import simpledialog, messagebox
 
 TOTAL_LUGARES = 83
 lugares = {i: None for i in range(1, TOTAL_LUGARES + 1)}
@@ -14,17 +16,20 @@ def mostrar_lugares():
             print(f"Lugar {i}: [LIVRE]")
 
 def registar_carro():
-    mat = input("Matrícula: ").upper()
+    mat = simpledialog.askstring("Registar Carro", "Matrícula:")
+    if mat is None or mat.strip() == "":
+        return
+    mat = mat.upper()
     for i, info in lugares.items():
         if info is None:
             agora = datetime.now()
             lugares[i] = {'matricula': mat, 'entrada': agora, 'pago': False, 'preco': 0.0}
-            print(f"Carro {mat} no lugar {i} às {agora.strftime('%H:%M')}")
+            messagebox.showinfo("Sucesso", f"Carro {mat} no lugar {i} às {agora.strftime('%H:%M')}")
             return
-    print("Parque cheio!")
+    messagebox.showwarning("Parque Cheio", "Não há lugares livres!")
 
 def marcar_pago():
-    global receita
+    global receita_total
     mat = input("Matrícula: ").upper()
     for info in lugares.values():
         if info and info['matricula'] == mat and not info['pago']:
@@ -33,7 +38,7 @@ def marcar_pago():
             preco = horas * 1.35
             info['pago'] = True
             info['preco'] = preco
-            receita += preco
+            receita_total += preco
             print(f"{mat}: {horas}h x 1.35€ = {preco:.2f} €")
             return
     print("Não encontrada ou já paga.")
@@ -52,15 +57,15 @@ def remover_carro():
     for i, info in lugares.items():
         if info and info['matricula'] == mat:
             if not info['pago']:
-                print("⚠️ Ainda não pagou.")
+                print("Ainda não pagou.")
             lugares[i] = None
             print(f"{mat} removido do lugar {i}.")
             return
     print("Matrícula não encontrada.")
 
 def mostrar_receita():
-       print(f"Receita total: {receita:.2f} €")
-       
+    print(f"Receita total: {receita_total:.2f} €")
+
 def menu():
     while True:
         print("\n=== MENU PARQUE ===")
@@ -91,4 +96,23 @@ def menu():
         else:
             print("Opção inválida.")
 
-menu()
+
+root = tk.Tk()
+root.title("Parque Estacionamento")
+root.attributes('-fullscreen', True)
+frame = tk.Frame(root)
+frame.pack(expand=True)
+btns = [
+    ("Ver lugares", mostrar_lugares),
+    ("Registar carro", registar_carro),
+    ("Marcar pago", marcar_pago),
+    ("Ver não pagos", listar_nao_pagos),
+    ("Remover carro", remover_carro),
+    ("Ver receita", mostrar_receita),
+    ("Sair", root.destroy)
+]
+
+for (text, cmd) in btns:
+    tk.Button(frame, text=text, width=40, height=4, command=cmd).pack(pady=10)
+
+root.mainloop()
